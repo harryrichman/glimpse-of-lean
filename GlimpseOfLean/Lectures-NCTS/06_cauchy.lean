948d344a824b7c4e053a74779f38f87ef152fd9a
Harry Richman <dhrichman@gmail.com>
2025-07-15 18:39:13 +0800

    make some progress, add informal proof sketches

--- GlimpseOfLean/Lectures-NCTS/06_cauchy.lean
+++ GlimpseOfLean/Lectures-NCTS/06_cauchy.lean
@@ -1,28 +1,34 @@
 import GlimpseOfLean.Library.Short
 import Mathlib.LinearAlgebra.Matrix.Spectrum
 
-def topMinor {n : ℕ} (A : Matrix (Fin (n+1)) (Fin (n+1)) ℝ) : Matrix (Fin n) (Fin n) ℝ :=
-  A.submatrix Fin.succ Fin.succ
+def topMinor {n : ℕ} {R : Type} [CommRing R] (A : Matrix (Fin (n+1)) (Fin (n+1)) R) : Matrix (Fin n) (Fin n) R :=
+  A.submatrix Fin.castSucc Fin.castSucc
 
-#eval topMinor !![5, 4; 3, 2]
+#eval topMinor !![(5 : ℤ), 4; 3, 2]
 
 theorem minorOfHermitianHermitian {n : ℕ} (A : Matrix (Fin (n+1)) (Fin (n+1)) ℝ) :
   (A.IsHermitian) → ((topMinor A).IsHermitian) := by
+  intro hA
   sorry
 
 
 -- Cauchy's Interlacing Theorem: If A is a real, symmetric nxn matrix, then the eigenvalues of any
 -- (n-1)x(n-1) minor interlace the eigenvalues of A
 
-theorem cauchy {n : ℕ} : ∀ A : Matrix (Fin (n+1)) (Fin (n+1)) ℝ,
-  (hA : A.IsHermitian) → ∀ i : Fin n, hA.eigenvalues i ≤ (minorOfHermitianHermitian A hA).eigenvalues i
+theorem cauchy {n : ℕ} : ∀ A : Matrix (Fin (n+1)) (Fin (n+1)) ℝ, (hA : A.IsHermitian) → ∀ i : Fin n,
+  hA.eigenvalues i ≤ (minorOfHermitianHermitian A hA).eigenvalues i
   ∧ (minorOfHermitianHermitian A hA).eigenvalues i ≤ hA.eigenvalues (i+1) := by
   sorry
+  -- Proof sketch:
+  -- The i-th eigenvalue of A is the maximum Rayleigh quotient over a subspace of R^(n+1) of
+  -- dimension i, when minimized over all possible subspaces of this dimension
+  -- The i-th eigenvalue of A's top minor is this same quantity, except minimized over subspaces
+  -- which are contained in the standard hyperplane R^n
 
 
 -- Small example: 2x2 case
 
-/- If A is a 2x2 symmetric real matrix, then the corner entry A_0,0 lies between the eigenvalues
+/- If A is a 2x2 symmetric real matrix, then the corner entry A_{0,0} lies between the eigenvalues
   of A -/
 theorem cauchy_2_2 (A : Matrix (Fin 2) (Fin 2) ℝ) (hA : A.IsHermitian) :
   (hA.eigenvalues 0 < A 0 0) ∧ (A 0 0 < hA.eigenvalues 1) := by
--- GlimpseOfLean/Lectures-NCTS/06_newton.lean
+++ GlimpseOfLean/Lectures-NCTS/06_newton.lean
@@ -1,19 +1,50 @@
 import GlimpseOfLean.Library.Short
 
+open Polynomial
+
 def logConcaveCoeff (p : Polynomial ℝ) : Prop :=
   ∀ i : Fin (p.natDegree), (p.coeff i)^2 ≥ (p.coeff (i - 1)) * (p.coeff (i + 1))
 
 def ultraLogConcaveCoeff (p : Polynomial ℝ) : Prop :=
-  ∀ i : Fin (p.natDegree), (p.coeff i)^2 ≥ (p.coeff (i - 1)) * (p.coeff (i + 1)) * (1 + 1.0/i) * (1 + 1.0/(p.natDegree - i))
+  ∀ i : Fin (p.natDegree),
+  (p.coeff i)^2 ≥ (p.coeff (i - 1)) * (p.coeff (i + 1)) * (1 + 1.0/i) * (1 + 1.0/(p.natDegree - i))
+
+noncomputable def binomCoeffToPoly (as : List ℝ) : Polynomial ℝ :=
+  let n := as.length
+  let monomials := List.map (fun (a, i) => C ((n.choose i) * a) * X^i) (List.zipIdx as)
+  monomials.sum
+
+#check List.zipIdx
+#check List.zip
 
 -- Newton's Theorem: If p(x) is a polynomial with real roots, then its coefficient sequence is
 -- ultra-log-concave
 
 theorem newton : ∀ p : Polynomial ℝ, (p.roots.card = p.natDegree) → ultraLogConcaveCoeff p := by
+  intro p
+  intro h
+  have hMon : p.Monic := by sorry
+  have hProd : _ := prod_multiset_X_sub_C_of_monic_of_roots_card_eq hMon h
   sorry
+  -- Proof sketch:
+  -- If p has all real roots, then the derivative p' also has all real roots
+  -- By repeatedly taking derivatives, we get a quadratic polynomial, which has real roots
+  -- By the quadratic formula, the ultra-log-concave relation holds for coeffs (n, n-1, n-2)
+  -- If p has all real roots, then so does the "dual" polynomial with reversed coefficients,
+  -- q(X) = X^n * p(1/X)
+  -- By applying the same argument to q, we get the ultra-log-concave relation for coeffs (0, 1, 2)
+  -- Finally,
 
 
 -- Small case: degree 2 polynomial
-theorem newton_2 (p : Polynomial ℝ) (hDeg : p.natDegree = 2) :
+theorem newton_2 (p : Polynomial ℝ) (hDeg : p.natDegree = 2) (hMon : p.Monic):
   (p.roots.card = 2) → (p.coeff 1)^2 ≥ 4*(p.coeff 0)*(p.coeff 2) := by
+  intro h
+  have hRoots : p.roots.card = p.natDegree := by
+    simp_all only
+  have h1 : ∃ a b k : ℝ, (p = (C k) * (X - C a) * (X - C b)) := by sorry
+  rcases h1 with ⟨a, b, k,  hconst⟩
+  have h2 : (p.coeff 2 = k) := by
+    simp_all only
+    sorry
   sorry
